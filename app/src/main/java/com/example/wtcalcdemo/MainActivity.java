7df29022282188a33e8b6663e88e7840d0605bd4
insanelygreatsoftware <102649951+insanelygreatsoftware@users.noreply.github.com>
2024-02-08 10:25:51 -0800

    commit

--- app/src/main/java/com/example/wtcalcdemo/MainActivity.java
+++ app/src/main/java/com/example/wtcalcdemo/MainActivity.java
@@ -92,6 +92,9 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
+//                github
+
+
                 txtViewResults.setText(formattedOutputWeight);
             }
         });
